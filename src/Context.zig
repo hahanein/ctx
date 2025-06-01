798be72d047a391883bf0c36762259eec4f36d9c
Benjamin Westphal <hahanein@users.noreply.github.com>
2025-06-01 23:40:47 +0200

    docs

--- src/Context.zig
+++ src/Context.zig
@@ -8,6 +8,7 @@ const diff = @import("diff.zig");
 paths: std.StringHashMap(void),
 merge_base: []const u8 = "",
 
+/// Initialize a new context.
 pub fn init(allocator: std.mem.Allocator) Context {
     return .{
         .paths = std.StringHashMap(void).init(allocator),
@@ -15,10 +16,12 @@ pub fn init(allocator: std.mem.Allocator) Context {
     };
 }
 
+/// Add paths to the context.
 pub fn add(self: *Context, paths: []const []const u8) !void {
     for (paths) |path| _ = try self.paths.put(path, {});
 }
 
+/// Remove paths from the context.
 pub fn rm(self: *Context, paths: []const []const u8) void {
     for (paths) |path| _ = self.paths.remove(path);
 }
